b887bea3a383f64f9632179aee648ae929e09f7e
Ben Wells <bwells@slb.com>
2017-10-12 21:38:56 +0100

    Output variables and use in plotting

--- plot_solution.m
+++ plot_solution.m
@@ -1,21 +1,34 @@
 clear all;
 
-output_t = 0.2
-numberReports = 10
+load 'variables.m';
 
-figure(1)
+output_t = variables(1);
+numberReports = variables(2);
+
 for report = 1:numberReports 
+
+   figure(report)
+   time = (report-1)* (output_t/(numberReports-1))
    
    filename = strcat('Solution', sprintf('%03d',[report]), '.m');
 
    u = load(filename);
-    
+   
+   subplot(2,2,1) 
    plot(u(:,1),u(:,2));
+   xlabel('x');
+   ylabel('\rho');   
+   title(strcat('Density (\rho) at time=',num2str(time)));
+   
+   subplot(2,2,2) 
+   plot(u(:,1),u(:,3));
+   xlabel('x');
+   ylabel('\rho u');   
+   title(strcat('Momentum (\rho u) at time=',num2str(time)));
 
-   hold on;
- end
-
- axis square;
- axis tight;
- xlabel('x');
- ylabel('u');
+   subplot(2,2,3) 
+   plot(u(:,1),u(:,4));
+   xlabel('x');
+   ylabel('E');
+   title(strcat('Energy (E) at time=',num2str(time)));
+  end
